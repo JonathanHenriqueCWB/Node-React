47ace14b83731dd373f1975001853c0d1be7d49f
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2021-05-10 19:59:44 -0300

    Cadastro no banco de dados

--- Curso-basico-node/Aula010-Handlebars/index.js
+++ Curso-basico-node/Aula010-Handlebars/index.js
@@ -1,42 +1,58 @@
-/**#################################<<< IMPORTS >>>####################################*/
-const express = require('express') // Express
+const express = require('express')
 const app = express()
-const Sequelize = require('sequelize') // ORM
-const handlebars = require('express-handlebars') // Template engine
+const handlebars = require('express-handlebars')
+const Post = require('./models/Post')
 
-/**##############################<<< CONFIGURAÇÃO >>>###################################*/
+// Confi handlebaras template engine
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+// Confi body-parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
-// Config
-    // Template Engine
-    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
-    app.set('view engine', 'handlebars')
-    // Nova versão do express não precisa do body-parser!
-    app.use(express.urlencoded({extended: false}))
-    app.use(express.json())
-    // Conexão com banco de dados MySql
-    const sequelize = new Sequelize('meu_blog', 'root', 'root', {
-        host: 'localhost',
-        dialect: 'mysql'
-    })
+// Rotas cadastrar
 
-/**##############################<<< ROTAS >>>#########################################*/
+// Rota Home
+app.get('/', function(req, res){
+    res.render('home')
+})
 
-app.get('/cad', function(req, res){
+// Rota de cadastro
+app.get('/cadastrar', function (req, res) {
     res.render('formulario')
 })
 
-app.post('/addPost', function(req, res) {
-    res.send("Titulo: " + req.body.titulo + " Conteudo: " + req.body.conteudo)
+app.post('/cadastrar', function (req, res) {
+    Post.create({
+        titulo: req.body.titulo,
+        conteudo: req.body.conteudo
+    }).then(function () {
+        res.redirect('/')
+    }).catch(function (erro) {
+        res.send('Erro no post: ' + erro)
+    })
 })
 
-/**##############################<<< SERVIDOR >>>######################################*/
-
-app.listen(8080, function(){
+// Servidor express
+app.listen(8080, function () {
     console.log('Server is runnig on port 8080')
 })
 
 /**
+ * Express é um popular framework web estruturado, escrito em JavaScript que roda sobre o ambiente node.js em tempo de execução.
+ * npm install --save express
+ *
  * handlebars é um template engine, serve para usar estruturas no html, tais como,
- * condicionais, repeticão e principalmente exibir dados que viram do backend no 
+ * condicionais, repeticão e principalmente exibir dados que viram do backend no
  * frontend.
+ * npm install --save express-hundlebars
+ *
+ * Sequelize é um ORM Object Relational Model, para banco de dados relacionais, tais como, PostgressSQL, MariaDB, SQLite e MySQL
+ * npm install --save sequelize
+ *
+ * Nodemon irá monitorar todas as alteraçãos nos arquivos de sua aplicação e reiniciar automaticamente o servidor quando for necessario
+ * npm install -g nodemon --save-dev
+ *
+ * mysql2 servidor de banco de dados mysql
+ * npm install --save mysql2
  */
